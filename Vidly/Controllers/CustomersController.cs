0407e5063dd777b6bef65ee939875a514eed5e01
fernastereo <fernandoecueto@gmail.com>
2019-11-09 12:54:20 -0500

    Changed the static data to querying objects

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -9,41 +9,55 @@ namespace Vidly.Controllers
 {
     public class CustomersController : Controller
     {
-        private List<Customer> customers = new List<Customer> {
-                //new Customer() { id = 1, name = "Tony Stark" },
-                //new Customer() { id = 2, name = "Steve Rogers" },
-                //new Customer() { id = 3, name = "Natasha Romanov" },
-                //new Customer() { id = 4, name = "Wanda Maximoff" },
-                //new Customer() { id = 5, name = "Nick Fury" },
-                };
+        private ApplicationDbContext _context;
+
+        public CustomersController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
+        private IEnumerable<Customer> getCustomers() {
+            return new List<Customer> {
+                new Customer() { id = 1, name = "Tony Stark" },
+                new Customer() { id = 2, name = "Steve Rogers" },
+                new Customer() { id = 3, name = "Natasha Romanov" },
+                new Customer() { id = 4, name = "Wanda Maximoff" },
+                new Customer() { id = 5, name = "Nick Fury" },
+            };
+        }
 
         // GET: Customers
-        public ActionResult Index(int? pageIndex, string sortBy)
+        public ActionResult Index()
         {
-            if (!pageIndex.HasValue)
-            {
-                pageIndex = 1;
-            }
-            if (string.IsNullOrWhiteSpace(sortBy))
-            {
-                sortBy = "Name";
-            }
+            var customers = _context.Customers.ToList();
 
             return View(customers);
         }
 
         public ActionResult Details(int id)
         {
-            var customer = from element in customers
-                           where element.id == id
-                           select element;
+            //var customer = from element in customers
+            //               where element.id == id
+            //               select element;
 
-            if (!customer.Any())
+            var customer = _context.Customers.SingleOrDefault(c => c.id == id);
+
+            //if (!customer.Any())
+            //{
+            //    return HttpNotFound();
+            //}
+
+            if (customer == null)
             {
                 return HttpNotFound();
             }
 
-            return View(customer.SingleOrDefault());
+            return View(customer);
         }
     }
 }
