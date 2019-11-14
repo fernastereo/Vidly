8dd1b00df6775704a84134bf48168a28b5410c03
fernastereo <fernandoecueto@gmail.com>
2019-11-14 16:59:57 -0500

    Eager loading between customer and membershipType

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -21,21 +22,11 @@ namespace Vidly.Controllers
             _context.Dispose();
         }
 
-        private IEnumerable<Customer> getCustomers() {
-            return new List<Customer> {
-                new Customer() { id = 1, name = "Tony Stark" },
-                new Customer() { id = 2, name = "Steve Rogers" },
-                new Customer() { id = 3, name = "Natasha Romanov" },
-                new Customer() { id = 4, name = "Wanda Maximoff" },
-                new Customer() { id = 5, name = "Nick Fury" },
-            };
-        }
-
         // GET: Customers
         public ActionResult Index()
         {
-            var customers = _context.Customers.ToList();
-
+            var customers = _context.Customers.Include(c => c.membershipType).ToList();
+            //include is for eager loading. When you try to retrieve data and a relationship whiit another model
             return View(customers);
         }
 
