ce9ad35e56d8d8501306d82b74b41e88679ff05e
fernastereo <fernandoecueto@gmail.com>
2019-11-18 14:30:46 -0500

    Create customer

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -34,6 +34,15 @@ namespace Vidly.Controllers
             return View(viewModel);
         }
 
+        [HttpPost]
+        public ActionResult Create(Customer customer)
+        {
+            _context.Customers.Add(customer);
+            _context.SaveChanges();
+
+            return RedirectToAction("index", "Customers");
+        }
+
         // GET: Customers
         public ActionResult Index()
         {
