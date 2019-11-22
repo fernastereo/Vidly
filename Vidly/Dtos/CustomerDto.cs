c7199e36bd6086d09364e2b6a80afa5bfcbd653f
fernastereo <fernandoecueto@gmail.com>
2019-11-22 17:51:08 -0500

    Refactoring CustomersController using Data Transfer Objects and AutoMapper

---
+++ Vidly/Dtos/CustomerDto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using Vidly.Models;
+
+namespace Vidly.Dtos
+{
+    public class CustomerDto
+    {
+        public int id { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string name { get; set; }
+        public bool isSubscribedToNewsLetter { get; set; }
+        public byte membershipTypeId { get; set; }
+        [Min18YearsIfAMember]
+        public DateTime? birthDate { get; set; }
+    }
+}
